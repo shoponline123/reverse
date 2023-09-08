4fa4baca9366ab8cc2b9da1326684ae6608cb88f
shoponline123 <144438711+shoponline123@users.noreply.github.com>
2023-09-08 02:53:22 -0700

    Update unzip.php

--- unzip.php
+++ unzip.php
@@ -1,40 +1,141 @@
+<?php
 /**
- * extract rar or zip file
+ * Class Extractor
  *
- * @since  [1.0]
- * @author Richard
- * @date   2015-05-05
- *
- * @param  [string]                      $file_path ["D:\file\path\a.rar"]
- * @param  [string]                      $to_path   ["D:\extract\to\path"]
- * @return [boolean]
+ * Extract a archive (zip/gzip/rar) file.
+ * 
+ * @author CodexWorld
+ * @url https://www.codexworld.com
+ * 
  */
-function extract_file($file_path, $to_path = "./")
-{
-        $file_type = substr($file_path, strrpos($file_path, '.') - strlen($file_path) + 1);
-        if ("zip" === $file_type) {
-        $xmlZip = new ZipArchive();
-        if ($xmlZip->open(__DIR__."/xml.zip") === true) {
-            $xmlZip->extractTo($to_path);
-            echo "extract success";
-        } else {
-            echo "extract fail";
-            return false;
-        }
-    } elseif ("rar" == $file_type) {
+class Extractor {
 
-        $rar_file = rar_open($file_path) or die("Can't open Rar archive");
-        $entries = rar_list($rar_file);
-        if ($entries) {
-                foreach ($entries as $entry) {
-                echo 'Filename: ' . $entry->getName() . "\n";
-                $entry->extract($to_path);
-            }
-            rar_close($rar_file);
-        } else{
-            echo "extract fail";
-            return false;
+    /**
+     * Checks file extension and calls suitable extractor functions.
+     *
+     * @param $archive
+     * @param $destination
+     */
+    public static function extract($archive, $destination){
+        $ext = pathinfo($archive, PATHINFO_EXTENSION);
+        switch ($ext){
+            case 'zip':
+                $res = self::extractZipArchive($archive, $destination);
+                break;
+            case 'gz':
+
+                $res = self::extractGzipFile($archive, $destination);
+                break;
+            case 'rar':
+
+                $res = self::extractRarArchive($archive, $destination);
+                break;
         }
 
+        return $res;
     }
+    
+    /**
+     * Decompress/extract a zip archive using ZipArchive.
+     *
+     * @param $archive
+     * @param $destination
+     */
+    public static function extractZipArchive($archive, $destination){
+        // Check if webserver supports unzipping.
+        if(!class_exists('ZipArchive')){
+            $GLOBALS['status'] = array('error' => 'Your PHP version does not support unzip functionality.');
+            return false;
+        }
+    
+        $zip = new ZipArchive;
+    
+        // Check if archive is readable.
+        if($zip->open($archive) === TRUE){
+            // Check if destination is writable
+            if(is_writeable($destination . '/')){
+                $zip->extractTo($destination);
+                $zip->close();
+                $GLOBALS['status'] = array('success' => 'Files unzipped successfully');
+                return true;
+            }else{
+                $GLOBALS['status'] = array('error' => 'Directory not writeable by webserver.');
+                return false;
+            }
+        }else{
+            $GLOBALS['status'] = array('error' => 'Cannot read .zip archive.');
+            return false;
+        }
+    }
+    
+    /**
+     * Decompress a .gz File.
+     *
+     * @param $archive
+     * @param $destination
+     */
+    public static function extractGzipFile($archive, $destination){
+        // Check if zlib is enabled
+        if(!function_exists('gzopen')){
+            $GLOBALS['status'] = array('error' => 'Error: Your PHP has no zlib support enabled.');
+            return false;
+        }
+    
+        $filename = pathinfo($archive, PATHINFO_FILENAME);
+        $gzipped = gzopen($archive, "rb");
+
+        $file = fopen($filename, "w");
+    
+        while ($string = gzread($gzipped, 4096)) {
+            fwrite($file, $string, strlen($string));
+        }
+        gzclose($gzipped);
+
+        fclose($file);
+    
+        // Check if file was extracted.
+        if(file_exists($destination.'/'.$filename)){
+            $GLOBALS['status'] = array('success' => 'File unzipped successfully.');
+            return true;
+        }else{
+            $GLOBALS['status'] = array('error' => 'Error unzipping file.');
+            return false;
+        }
+    }
+    
+    /**
+     * Decompress/extract a Rar archive using RarArchive.
+     *
+     * @param $archive
+     * @param $destination
+
+     */
+    public static function extractRarArchive($archive, $destination){
+        // Check if webserver supports unzipping.
+        if(!class_exists('RarArchive')){
+            $GLOBALS['status'] = array('error' => 'Your PHP version does not support .rar archive functionality.');
+            return false;
+        }
+        // Check if archive is readable.
+        if($rar = RarArchive::open($archive)){
+
+            // Check if destination is writable
+            if (is_writeable($destination . '/')) {
+                $entries = $rar->getEntries();
+                foreach ($entries as $entry) {
+                    $entry->extract($destination);
+                }
+                $rar->close();
+                $GLOBALS['status'] = array('success' => 'File extracted successfully.');
+                return true;
+            }else{
+                $GLOBALS['status'] = array('error' => 'Directory not writeable by webserver.');
+                return false;
+            }
+        }else{
+            $GLOBALS['status'] = array('error' => 'Cannot read .rar archive.');
+            return false;
+        }
+    }
+    
 }
